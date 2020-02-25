212f511abc2cadcdef0de5ac360ed2fada9fb7dc
Kirsten Keels <keelsk@gmail.com>
2020-02-25 03:57:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
   small_key = ""
   name_hash.each do |key, value|
     if value < minimum
-      small_key = "#{key}"
+      small_key = ":#{key}"
     end
   end
   small_key
